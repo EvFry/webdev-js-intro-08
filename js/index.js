8efb1e290236052fa07a5894b3b12307a77025b4
Evlen Fryling <evlen.fryling@gmail.com>
2025-01-17 18:30:16 -0500

    Task 1 complete

--- js/index.js
+++ js/index.js
@@ -1,3 +1,48 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/* 
+const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+*/
+/*
+for (const fruit of fruits) {
+    console.log(fruit);
+}
+const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+fruits.forEach(fruit => {
+    console.log(fruit);
+});
+*/
+/*
+let fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+for (const index in fruits) {
+    fruits[index] = fruits[index].toUpperCase();
+}
+
+console.log(fruits);
+
+*/
+/*
+let fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+fruits.forEach((fruit, index, arr) => {
+    arr[index] = fruit.toUpperCase();
+});
+*/
+
+/*
+console.log(fruits);
+
+const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+
+for (const index in fruits) {
+    console.log(`Index ${index}: ${fruits[index]}`);
+}
+    */
